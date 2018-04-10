f54869dba8ea26207e6013ea00c4e6be8e22819d
Jerridd <jerridd_speidel@yahoo.com>
2018-04-10 15:01:10 -0500

    have a layout 4x3 in the bootstrap column.

--- clickygame/src/components/FriendCard/FriendCard.js
+++ clickygame/src/components/FriendCard/FriendCard.js
@@ -2,15 +2,14 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  // <div className="card-columns">
-  <div className="card-columns">
+
   <div className="card">
     <div className="img-container ">
       <img alt={props.name} src={props.image} />
     </div>
+
   </div>
-  </div>
-  // </div>
+
 );
 
 export default FriendCard;
