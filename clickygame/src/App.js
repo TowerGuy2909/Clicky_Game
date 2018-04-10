9e62da00b288b93af1689e2e57896bf56b186933
Jerridd <jerridd_speidel@yahoo.com>
2018-04-10 11:51:13 -0500

    Forced images into a 4 x 3 grid at full screen. Doesn't work smaller screen.

---
+++ clickygame/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import "./App.css";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
