38b063d78c18f65a3971617211c79b303a4379ee
Jerridd <jerridd_speidel@yahoo.com>
2018-04-13 17:32:48 -0500

    changed props to have a clickHandler to run the code when clicked.

--- clickygame/src/components/FriendCard/FriendCard.js
+++ clickygame/src/components/FriendCard/FriendCard.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./FriendCard.css";
 
-const FriendCard = props => (
-
-  <div className="card">
+const FriendCard = props => {
+ return <div className="card" 
+  onClick= {props.clickHandler}
+  >
     <div className="img-container ">
       <img alt={props.name} src={props.image} />
     </div>
-
   </div>
-
-);
+};
 
 export default FriendCard;
